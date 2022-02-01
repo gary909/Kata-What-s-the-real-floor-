c22fe9da4be0eab75b3c8e807350b6cef988a353
gary <gary.white@lodgit.com>
2022-02-01 13:12:34 +0100

    code working

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function getRealFloor(n) {
+    if (n < 1) {
+        return n;
+    } else if (n >= 1 && n <= 13) {
+        return n - 1;
+    } else if (n > 13) {
+        return n - 2;
+    }
 }
 
 console.log(getRealFloor(1)); // return 0
